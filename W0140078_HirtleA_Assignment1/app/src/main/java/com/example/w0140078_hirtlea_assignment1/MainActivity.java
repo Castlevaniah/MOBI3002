e4567b0cdd5e080d4ae8b94816248a18a4b47dff
Castlevaniah <aaronhirtle85@gmail.com>
2025-09-19 10:45:23 -0300

    first commit

---
+++ W0140078_HirtleA_Assignment1/app/src/main/java/com/example/w0140078_hirtlea_assignment1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.w0140078_hirtlea_assignment1;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Log.i("STACK_OVERFLOW_LOG", "HELLO FROM THE OTHERSIDE");
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
