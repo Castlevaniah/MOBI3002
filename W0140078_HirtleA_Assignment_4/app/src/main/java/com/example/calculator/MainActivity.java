f0347eca8ec52d5938a2da0f756c94a6c8797127
Aaron Hirtle <aaronhirtle85@gmail.com>
2025-10-01 14:17:13 -0300

    latest updates to buttons

--- W0140078_HirtleA_Assignment_4/app/src/main/java/com/example/calculator/MainActivity.java
+++ W0140078_HirtleA_Assignment_4/app/src/main/java/com/example/calculator/MainActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView display;
+    private EditText inputField;
     private String currentInput = "";
     private double firstNumber = 0;
     private String operator = "";
@@ -19,98 +19,115 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        display = findViewById(R.id.display);
+        inputField = findViewById(R.id.inputField);
+
+        // Restore state if activity restarted
+        if (savedInstanceState != null) {
+            currentInput = savedInstanceState.getString("currentInput", "");
+            firstNumber = savedInstanceState.getDouble("firstNumber", 0);
+            operator = savedInstanceState.getString("operator", "");
+            inputField.setText(savedInstanceState.getString("displayText", ""));
+        }
 
         // Number buttons
-        int[] numberButtons = {R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4,
-                R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9};
+        int[] numberButtons = {
+                R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4,
+                R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9
+        };
 
         for (int id : numberButtons) {
             Button btn = findViewById(id);
-            btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Button b = (Button) view;
-                    currentInput += b.getText().toString();
-                    display.setText(currentInput);
+            btn.setOnClickListener(view -> {
+                Button b = (Button) view;
+                currentInput += b.getText().toString();
+                inputField.setText(currentInput);
 
-                    // Log number pressed
-                    Log.d("CalculatorApp", "Number pressed: " + b.getText().toString() +
-                            " | Current input: " + currentInput);
-                }
+                Log.d("CalculatorApp", "Number pressed: " + b.getText().toString() +
+                        " | Current input: " + currentInput);
             });
         }
 
         // Operator buttons
-        int[] operatorButtons = {R.id.btnAdd, R.id.btnSubtract, R.id.btnMultiply, R.id.btnDivide};
+        int[] operatorButtons = {
+                R.id.btnPlus, R.id.btnMinus, R.id.btnMultiply, R.id.btnDivide
+        };
+
         for (int id : operatorButtons) {
             Button btn = findViewById(id);
-            btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    firstNumber = Double.parseDouble(currentInput);
-                    operator = ((Button) view).getText().toString();
-                    currentInput = "";
-                    display.setText("");
-
-                    // Log operator pressed
-                    Log.d("CalculatorApp", "Operator selected: " + operator +
-                            " | First number: " + firstNumber);
+            btn.setOnClickListener(view -> {
+                if (!inputField.getText().toString().isEmpty()) {
+                    currentInput = inputField.getText().toString();
                 }
+                if (!currentInput.isEmpty()) {
+                    firstNumber = Double.parseDouble(currentInput);
+                }
+                operator = ((Button) view).getText().toString();
+                currentInput = "";
+                inputField.setText("");
+
+                Log.d("CalculatorApp", "Operator selected: " + operator +
+                        " | First number: " + firstNumber);
             });
         }
 
         // Equals button
-        Button btnEquals = findViewById(R.id.btnEquals);
-        btnEquals.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                double secondNumber = Double.parseDouble(currentInput);
-                double result = 0;
-
-                switch (operator) {
-                    case "+":
-                        result = firstNumber + secondNumber;
-                        break;
-                    case "-":
-                        result = firstNumber - secondNumber;
-                        break;
-                    case "*":
-                        result = firstNumber * secondNumber;
-                        break;
-                    case "/":
-                        if (secondNumber != 0) {
-                            result = firstNumber / secondNumber;
-                        } else {
-                            display.setText("Error");
-                            Log.e("CalculatorApp", "Division by zero attempted!");
-                            return;
-                        }
-                        break;
-                }
-
-                display.setText(String.valueOf(result));
-                currentInput = String.valueOf(result);
-
-                // Log calculation
-                Log.i("CalculatorApp", "Calculation performed: " +
-                        firstNumber + " " + operator + " " + secondNumber + " = " + result);
+        Button btnEqual = findViewById(R.id.btnEqual);
+        btnEqual.setOnClickListener(view -> {
+            if (!inputField.getText().toString().isEmpty()) {
+                currentInput = inputField.getText().toString();
             }
+            if (currentInput.isEmpty()) return;
+
+            double secondNumber = Double.parseDouble(currentInput);
+            double result = 0;
+
+            switch (operator) {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "−":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "×":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "÷":
+                    if (secondNumber != 0) {
+                        result = firstNumber / secondNumber;
+                    } else {
+                        inputField.setText("Error");
+                        Log.e("CalculatorApp", "Division by zero attempted!");
+                        return;
+                    }
+                    break;
+            }
+
+            inputField.setText(String.valueOf(result));
+            currentInput = String.valueOf(result);
+
+            Log.i("CalculatorApp", "Calculation: " +
+                    firstNumber + " " + operator + " " + secondNumber + " = " + result);
         });
 
         // Clear button
         Button btnClear = findViewById(R.id.btnClear);
-        btnClear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                currentInput = "";
-                firstNumber = 0;
-                operator = "";
-                display.setText("");
+        btnClear.setOnClickListener(view -> {
+            currentInput = "";
+            firstNumber = 0;
+            operator = "";
+            inputField.setText("");
 
-                // Log clear action
-                Log.w("CalculatorApp", "Calculator cleared");
-            }
+            Log.w("CalculatorApp", "Calculator cleared");
         });
+    }
+
+    // Save state for rotation / restart
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("currentInput", currentInput);
+        outState.putDouble("firstNumber", firstNumber);
+        outState.putString("operator", operator);
+        outState.putString("displayText", inputField.getText().toString());
     }
 }
